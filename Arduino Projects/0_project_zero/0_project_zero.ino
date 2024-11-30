897a5ecb884c3cd31ab0e2cc9f16989b02985c58
aclandmark <mark.osborne15@btinternet.com>
2024-11-30 20:30:36 +0000

    Create 0_project_zero.ino

---
+++ Arduino Projects/0_project_zero/0_project_zero.ino
@@ -0,0 +1,12 @@
+
+
+//Our very first project.
+
+//Why not copy the C code from one or two examples in the folder "C_file_images\0_project_zero"
+//Start typing bellow the "#include" command.
+//People often use a project like one of these to test that everything has been set up correctly
+//especially if they are not familiiar with the microchip and programming hardware.
+
+#include "Project_0_header.h"
+
+ 
