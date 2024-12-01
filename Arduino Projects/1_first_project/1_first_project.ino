f3a9e9fa54e65113caeb3f57d95d864ec169ab56
aclandmark <mark.osborne15@btinternet.com>
2024-12-01 21:17:40 +0000

    Update 1_first_project.ino

--- Arduino Projects/1_first_project/1_first_project.ino
+++ Arduino Projects/1_first_project/1_first_project.ino
@@ -2,14 +2,8 @@
 
 //Some more very basic projects, this time generating simple patterns on the display 
 //some of them regular and repeating and some of the random.
-//C code can be found in the folder "C_file_images\0_project_zero"
-//
-
-//Why not copy the C code from one or two examples in the folder "C_file_images\1_first_project"
+//Why not copy the C code for one or two examples to be found in folder "C_file_images\1_first_project"
 
 
 
 #include "First_project_header.h"
-
-
-  
