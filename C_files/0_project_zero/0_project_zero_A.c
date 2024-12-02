5af3d6c4fe273115df9e6bc64ad16dc4756ec726
aclandmark <mark.osborne15@btinternet.com>
2024-12-02 20:29:33 +0000

    Update 0_project_zero_A.c

--- C_files/0_project_zero/0_project_zero_A.c
+++ C_files/0_project_zero/0_project_zero_A.c
@@ -1,17 +1,23 @@
 
-
 int main (void)  	//Example A  Red LED flashes
 {  setup_HW_Basic;
   LED_1_on;
   SW_reset;}
 
 
-
-int main (void)  	//Example B  Red LED flashes
+int main (void)  	//Example B_1  Red LED flashes
 { setup_HW_Basic;
   while(1){
   switch_LED_1;
   _delay_ms(50);}}
+
+
+int main (void)  	//Example B_2  Red LED flashes 
+{  setup_HW_Basic;	//when switch_1 is pressed
+  while (1) {
+    while (switch_1_up);
+    {switch_LED_1; }
+    Timer_T0_10mS_delay_x_m(20);  }}
 
 
 
