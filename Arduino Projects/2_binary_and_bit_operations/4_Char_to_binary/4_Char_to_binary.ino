a6ba95fb40bc541f515ca67aa224af9d38e573be
aclandmark <mark.osborne15@btinternet.com>
2024-12-03 19:45:40 +0000

    Update 4_Char_to_binary.ino

--- Arduino Projects/2_binary_and_bit_operations/4_Char_to_binary/4_Char_to_binary.ino
+++ Arduino Projects/2_binary_and_bit_operations/4_Char_to_binary/4_Char_to_binary.ino
@@ -16,7 +16,6 @@ op=0;
 mode = 'u';                                               //mode: signed or unsigned
 setup_HW;
 I2C_Tx_display_char(digit,mode);
-//setup_and_enable_PCI;
 
 set_up_PCI;
 enable_pci;
@@ -32,14 +31,6 @@ case 2: I2C_Tx_display_char(--digit, mode);break;}        //Decrement "digit" be
 Timer_T0_10mS_delay_x_m(15);}}
 
 
-/**************************************************************************************************/
-ISR(PCINT2_vect) {                                        //sw1 and sw3 interrupt
-if(switch_2_down)return;                                  //Ignore if sw3 is still down
-if((switch_1_up) && (switch_3_up)){op = 0; return;}       //Both switches up
-
-if(switch_1_down) {op = 1; return;}                       //Digits increment
-if(switch_3_down) {op = 2; return;}}                      //Digits decrement
-
 
 /**************************************************************************************************/
 ISR(PCINT0_vect){                                          //sw3 interrupt service routine
@@ -49,6 +40,17 @@ if (mode == 's')
 {mode = 'u'; I2C_Tx_display_char(digit,mode);return;}      //toggle display from signed to unsigned
 if (mode == 'u')
 {mode = 's'; I2C_Tx_display_char(digit,mode);return;}}     //and visa-versa
+
+
+
+ISR(PCINT2_vect) {                                        //sw1 and sw3 interrupt
+if(switch_2_down)return;                                  //Ignore if sw3 is still down
+if((switch_1_up) && (switch_3_up)){op = 0; return;}       //Both switches up
+
+if(switch_1_down) {op = 1; return;}                       //Digits increment
+if(switch_3_down) {op = 2; return;}}                      //Digits decrement
+
+
 
 
 
