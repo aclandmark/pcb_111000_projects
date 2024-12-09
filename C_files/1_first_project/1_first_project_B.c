f0e9523dbffcd9f10efc7dcc5cb9087829ab1c7a
aclandmark <mark.osborne15@btinternet.com>
2024-12-09 17:50:27 +0000

    Update 1_first_project_B.c

--- C_files/1_first_project/1_first_project_B.c
+++ C_files/1_first_project/1_first_project_B.c
@@ -1,6 +1,6 @@
 #include "Local_subroutines.c"
 
-int main (void)          //Example 2A
+int main (void)          //Example 4
   { unsigned int PORT_1;
     int T1_period = 15;
 
@@ -17,13 +17,12 @@ for (int m = 14; m; m--)
     Timer_T0_10mS_delay_x_m(T1_period); }
    if(switch_1_down)T1_period = 
    Adjust_T1_period(T1_period);}}
- 
-  
-  
-  
-   int main (void)    //Example 3
+   
+   
+   
+   
+   int main (void)    //Example 5
   { unsigned int PORT_1, PORT_2;
-
   setup_HW;
   while (1)
   { PORT_1 = 0b0000000000000001; 
@@ -33,5 +32,23 @@ for (int m = 14; m; m--)
       _delay_ms(60);
       PORT_1 = PORT_1 << 1;
       PORT_2 = PORT_2 >> 1;
-    }}
-  return 1;  }
+    }}return 1;  }
+	
+	
+	
+	
+	int main (void)               //Example 6
+  { unsigned long PORT_1, PORT_2;
+
+  setup_HW;
+  while (1)
+  { PORT_1 = 1;
+    PORT_2 = 0x80000000;
+    for (int m = 0; m <= 31; m++)
+    {  I2C_Tx_2_integers(PORT_1, PORT_2);
+      _delay_ms(60);
+      PORT_1 = PORT_1 << 1;
+      PORT_2 = PORT_2 >> 1;
+    }}return 1;}
+	
+	
