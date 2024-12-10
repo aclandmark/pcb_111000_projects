d9f78484f248e0843d24021b6e49ec6623300a3f
aclandmark <mark.osborne15@btinternet.com>
2024-12-10 21:04:17 +0000

    Update 1_askii_code.ino

--- Arduino Projects/4_Receiver_Transmitter_Basic/1_askii_code/1_askii_code.ino
+++ Arduino Projects/4_Receiver_Transmitter_Basic/1_askii_code/1_askii_code.ino
@@ -1,26 +1,14 @@
 
 
+//Use cntrl z
+//put numbers in order
 
 
 #include "Receiver_Transmitter_header.h"
 #include "Local_subroutines.c"
 
-  int main (void)                          //Example 1
-  { setup_HW;
-  char keypress;
- 
-  if (watch_dog_reset == 1) String_to_PC_Local("\r\n?  ");
-  else {User_prompt;String_to_PC_Local("\r\nProgram to echo keypresses:\r\n\
-Enter number; -cr- when done\r\n    ");}
-  
-  while (1)
-  { keypress = waitforkeypress_Local();
-    if  ((keypress =='\r') ||  (keypress =='\n'))break; 
-    else Char_to_PC_Local(keypress); }
- SW_reset; }
-  
-//Do caps to lowercase and reverse
-//Use cntrl z
+     
+
 
 /************************************************************************************************************
 
@@ -33,7 +21,7 @@ Enter number; -cr- when done\r\n    ");}
   int main (void)                          //Example 1
   { setup_HW;
   User_prompt;
-  String_to_PC_Local("\r\nExploring the operation oof the USART\r\n");
+  String_to_PC_Local("\r\nExploring the operation of the USART\r\n");
   Char_to_PC_Local('?');
   newline_Basic();
   while (1)
@@ -44,10 +32,25 @@ Enter number; -cr- when done\r\n    ");}
   }
 
 
+int main (void)                          //Example 2
+  { setup_HW;
+  char keypress;
+ 
+  if (watch_dog_reset == 1) String_to_PC_Local("\r\n?  ");
+  else {User_prompt;String_to_PC_Local("\r\nWe often send\
+  detailed text the first time our program runs\r\n\
+  and a simple prompt on subsequent ocaissions\r\n\
+  Enter number -cr- when done\r\n    ");}
+  
+  while (1)
+  { keypress = waitforkeypress_Local();
+    if  ((keypress =='\r') ||  (keypress =='\n'))break; 
+    else Char_to_PC_Local(keypress); }
+ SW_reset; }
 
 
-**********Example 2: Prints out ASKII characters**************************************************************
-  int main (void)                          //Example 2
+**********Example 3: Prints out ASKII characters**************************************************************
+  int main (void)                          //Example 3
   { char symbol;
   setup_HW;
   newline_Basic();
@@ -66,8 +69,8 @@ Enter number; -cr- when done\r\n    ");}
 
 
 
-********Example 3: Echo character string or prints file*******************************************************
-int main (void)                          //Example 3
+********Example 4: Echo character string or prints file*******************************************************
+int main (void)                          //Example 4
   { setup_HW;
   while (!(isCharavailable_Local(65)))    //Use local version
     Char_to_PC_Local('?');                //Use local version
@@ -84,10 +87,32 @@ int main (void)                          //Example 3
  
 
 
+//Look for text strings
+int main (void)                          //Example 5
+  { setup_HW;
+  char keypress;
+ 
+  String_to_PC_Basic("abcdefghijklmnopqrstuvwxyz");
+ String_to_PC_Basic("\r\nSend_the_hex_file\r\n");
+
+ while (!(isCharavailable_Local(65)))    //Use local version
+    Char_to_PC_Local('?');                //Use local version
+  newline_Basic();
+  Char_to_PC_Local(Char_from_PC_Local());
+  while (1)
+  { if (isCharavailable_Local(10))
+      Char_to_PC_Local(Char_from_PC_Local());
+    else break;
+  }
+ String_to_PC_Local("Done\r\n");
+ 
+ 
+ while(1); }
+********************************************************************************************************
 
 
-********Example 4: Send strings to the PC********************************************************************
-int main (void)                          //Example 4
+********Example 6: Send strings to the PC********************************************************************
+int main (void)                          //Example 6
   { setup_HW;
   wdt_enable(WDTO_120MS);
   while(switch_1_down)wdr();
@@ -109,8 +134,8 @@ int main (void)                          //Example 4
 
 
 
-************Example 5: Generate an ASKII table****************************************************************
-int main (void)                          //Example 5
+************Example 7: Generate an ASKII table****************************************************************
+int main (void)                          //Example 7
   { char symbol = '!';
   setup_HW;
   wdt_enable(WDTO_120MS);
@@ -128,9 +153,24 @@ int main (void)                          //Example 5
   while(switch_1_down);
   }
   
+  //Lower case to upper case
+int main (void)                          //Example 8
+  { setup_HW;
+  char keypress;
+ char text[25];
+ int m=0;
+ 
+  if (watch_dog_reset == 1) String_to_PC_Local("\r\n?  ");
+  else {User_prompt;String_to_PC_Local("\r\nEnter some text\
+  maybe the name of a song then -cr-\r\n");}
   
-
-
-
-
-/********************************************************************************************************/
+  while (1)
+  { keypress = waitforkeypress_Local();
+    if  ((keypress =='\r') ||  (keypress =='\n'))break;
+    else {Char_to_PC_Local(keypress); text[m++] = keypress; }}
+text [m] = '\0';
+m=0;
+while(text[m]){if((text[m]>='a') && (text[m]<= 'z'))
+text[m] -= 32; m+=1;}
+newline_Basic(); String_to_PC_Local(text);
+SW_reset; }
