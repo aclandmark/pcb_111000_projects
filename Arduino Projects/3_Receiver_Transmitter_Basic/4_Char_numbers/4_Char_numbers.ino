aead91056269f070520b3f640d749498b635507e
aclandmark <mark.osborne15@btinternet.com>
2024-12-13 15:57:18 +0000

    Create 4_Char_numbers.ino

---
+++ Arduino Projects/3_Receiver_Transmitter_Basic/4_Char_numbers/4_Char_numbers.ino
@@ -0,0 +1,45 @@
+
+
+
+
+#include "Proj_2_C1_header_file.h"
+#include "Local_subroutines.c"
+
+
+
+ 
+
+
+
+int main (void){
+char op=0;
+char mode;
+char digit=1;
+mode = 'u';                         //mode: signed or unsigned
+setup_HW;
+I2C_Tx_display_char(digit,mode);update_screen(mode,digit);
+
+
+while(1){
+if(isCharavailable_Basic(1))op = Char_from_PC_Basic();
+  
+switch(op){
+case 0: break;                        //Static display
+case 'x': I2C_Tx_display_char(++digit,mode);update_screen(mode,digit); break;
+case 'y': I2C_Tx_display_char(--digit, mode);update_screen(mode,digit);break; 
+case 'X': digit += 4;I2C_Tx_display_char(digit,mode);update_screen(mode,digit); break;
+case 'Y': digit -= 4;I2C_Tx_display_char(digit,mode);update_screen(mode,digit); break;
+case 'z':
+case 'Z': if(mode == 'u')mode = 's'; else mode = 'u';
+I2C_Tx_display_char(digit,mode);update_screen(mode,digit);break;
+default: String_to_PC_Basic("\r\nStarting again.\r\n");SW_reset;break;}
+Timer_T0_10mS_delay_x_m(15);
+op = 0;}}
+
+
+
+
+
+
+
+/**************************************************************************************************/
