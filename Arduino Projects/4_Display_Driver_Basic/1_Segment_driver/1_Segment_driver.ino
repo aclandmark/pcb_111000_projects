8acfb887baf4c031ae59fc74e6fe1c8ed16cef42
aclandmark <mark.osborne15@btinternet.com>
2024-12-14 20:50:43 +0000

    Create 1_Segment_driver.ino

---
+++ Arduino Projects/4_Display_Driver_Basic/1_Segment_driver/1_Segment_driver.ino
@@ -0,0 +1,53 @@
+
+/*
+ Enables users to drive the display manually
+ This indicates that there is not necessarily any connection between 
+ the display and an actual number.
+ */
+
+#include "Segment_driver_header.h"
+
+
+#define message_1 "\r\nPress key a to g (x for next digit).\r\n\
+Press 'z' to clear the display.\r\n"
+#define message_2 "?\r\n"
+
+
+
+int main (void){
+
+char letter = 0, digit_num;            
+
+setup_HW;
+
+
+I2C_Tx_any_segment_clear_all();
+
+if (watch_dog_reset != 1)String_to_PC_Basic(message_1);
+else String_to_PC_Basic(message_2);
+
+while(1){digit_num=0;
+while(digit_num < 8){
+
+if(isCharavailable_Basic(1))                                  //Wait up to 25mS for a key press.  In the absence of one
+{switch(letter = Char_from_PC_Basic()){                              //the program reverts back to "while(digit_num < 8){"
+case 'a': case 'A':                                           //otherwise it continues and updates the display
+case 'b': case 'B':                                           //Program execution is not put on hold awaiting a keypress
+case 'c': case 'C':
+case 'd': case 'D':
+case 'e': case 'E':
+case 'f': case 'F':
+case 'g': case 'G': 
+I2C_Tx_any_segment(letter, digit_num); break;
+case 'x': case 'X': digit_num += 1;break;
+case 'z': SW_reset;break;
+default: break;}}
+_delay_ms(25);
+wdr();
+}}}
+
+
+
+
+
+/********************************************************************************************************************/
