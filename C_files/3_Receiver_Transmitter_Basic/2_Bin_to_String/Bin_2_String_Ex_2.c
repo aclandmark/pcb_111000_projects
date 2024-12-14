7b7d616b18a202e578b5c15d77eaecea1485bf68
aclandmark <mark.osborne15@btinternet.com>
2024-12-14 18:16:54 +0000

    Create Bin_2_String_Ex_2.c

---
+++ C_files/3_Receiver_Transmitter_Basic/2_Bin_to_String/Bin_2_String_Ex_2.c
@@ -0,0 +1,46 @@
+
+
+
+
+
+int main (void)                 //Example 2
+  { long number = 1;			//Print out a column of numbers
+  long number_old = 1;
+ 
+  setup_HW;
+  wdt_enable(WDTO_120MS);
+  while(switch_1_down)wdr();
+  
+  for(int m = 2; m < 11; m++){
+  Int_to_PC_Basic(number);
+ newline_Basic();
+
+number = (number_old * 10) + m;  
+number_old = number;}
+
+while(switch_1_up)wdr();
+  while(switch_1_down);
+  return 1;}
+  
+  
+  
+  
+  int main (void)                    //Example 3
+  { int i = 0; 						//Print out a column of numbers
+  long number = 123456789;
+  long number_old = 123456789;
+  char s[12];
+  setup_HW;
+   wdt_enable(WDTO_120MS);
+  while(switch_1_down)wdr();
+  
+  for(int m = 2; m < 11; m++){
+Int_to_PC_Basic(number);
+ newline_Basic();
+
+number = number_old / 10;  
+number_old = number;}
+
+while(switch_1_up)wdr();
+  while(switch_1_down);
+  return 1;}
