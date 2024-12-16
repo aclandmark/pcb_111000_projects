cfe4b66e5dd7d12a16e0eaf3ac3d8c9e8f4735eb
aclandmark <mark.osborne15@btinternet.com>
2024-12-16 17:32:55 +0000

    Update 2_Bin_to_String.ino

--- Arduino Projects/3_Receiver_Transmitter_Basic/2_Bin_to_String/2_Bin_to_String.ino
+++ Arduino Projects/3_Receiver_Transmitter_Basic/2_Bin_to_String/2_Bin_to_String.ino
@@ -5,33 +5,9 @@
 #include "Receiver_Transmitter_header.h"
 #include "Local_subroutines.c"
 
-int Bin_to_string_Local(long, char *);
 
 
-
-int main (void)                          //Example 1B 
-  { unsigned int random_num; 
-    unsigned char PRN_counter;
-  char num_string[12];
- int str_len;
- int num_counter =0;
-
-  setup_HW; 
-  PRN_counter = 0;
-  random_num = PRN_16bit_GEN (0, &PRN_counter);
-
-  while(1){
- str_len = Bin_to_string_Local(random_num%100, num_string);
- Num_string_to_PC_Local(num_string, str_len);
-waitforkeypress_Basic(); num_counter += 1; 
-if(!(num_counter%15))newline_Basic();else Char_to_PC_Basic(' ');
-random_num = PRN_16bit_GEN (random_num, &PRN_counter);}
-return 1;}
-
-
-
-/*
-int main (void)                          //Example 1 
+ int main (void)              //Example 1 
   { int i = 0;              //Convert number to string
   int number = 12345;
   char s[12];
@@ -40,7 +16,7 @@ int main (void)                          //Example 1
   wdt_enable(WDTO_120MS);
  while(switch_1_down)wdr();
 
-  //Convert number to string
+  //Convert binary number to string
   while(1) {
     s[i++] = number % 10 + '0';
     number = number/10;
@@ -55,29 +31,6 @@ int main (void)                          //Example 1
  while(switch_1_up)wdr();
   while(switch_1_down);
   return 1;}
-*/
-
-/*int main (void)                          //Example 1A
-  {                                      //Convert number to string
-  long number = 2;
- char num_string[12];
- char direction = 0;
- int str_len;
   
-  setup_HW;
- 
-while(number) {
-str_len = Bin_to_string_Local(number, num_string);
- Num_string_to_PC_Local(num_string, str_len);
- if (!(direction))number = number *3/2;
-else number = number *2/3;
-  
-  newline_Basic();
- waitforkeypress_Basic();
-if (number >= 18000){direction ^= 1;
-number = number *2/3;newline_Basic();}}
-newline_Basic();SW_reset;
-  return 1;}*/
-
 
 /*************************************************************************************************************************/
