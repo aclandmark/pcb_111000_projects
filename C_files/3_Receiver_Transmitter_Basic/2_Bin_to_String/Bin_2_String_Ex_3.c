7d8f65fd10c6cb8ad1b9b0080b281c8e0220001f
aclandmark <mark.osborne15@btinternet.com>
2024-12-18 15:43:28 +0000

    Update Bin_2_String_Ex_3.c

--- C_files/3_Receiver_Transmitter_Basic/2_Bin_to_String/Bin_2_String_Ex_3.c
+++ C_files/3_Receiver_Transmitter_Basic/2_Bin_to_String/Bin_2_String_Ex_3.c
@@ -1,4 +1,7 @@
 
+#include "Local_subroutines.c"
+
+
 
 
 int main (void)                          //Example 5 
@@ -7,24 +10,31 @@ int main (void)                          //Example 5
     unsigned int Short_random_number; 
   char num_string[12];
  int str_len;
- int num_counter =0;
+  int num_array[40];
 
   setup_HW; 
   PRN_counter = 0;
   random_num = PRN_16bit_GEN (0, &PRN_counter);
   Short_random_number = PRN_8bit_GEN();
-  
-  while(1){
-switch(Short_random_number%4)
- {case 0:str_len = Bin_to_string_Local(random_num%10,     num_string);break;
-  case 1:str_len = Bin_to_string_Local(random_num%100,    num_string);break;
-  case 2:str_len = Bin_to_string_Local(random_num%1000,    num_string);break;
-  case 3:str_len = Bin_to_string_Local(random_num%10000,  num_string);break;}
 
+while(1){
+  
+ for(int m = 0; m <40; m++){
+switch(Short_random_number%4)
+ {case 0:num_array[m] = random_num%10; break;
+  case 1:num_array[m] = random_num%100; break;
+  case 2:num_array[m] = random_num%1000; break;
+  case 3:num_array[m] = random_num%10000; break;}
+
+str_len = Bin_to_string_Local(num_array[m], num_string);
  Num_string_to_PC_Local(num_string, str_len);
-waitforkeypress_Basic(); num_counter += 1; 
-if(!(num_counter%15))newline_Basic();else Char_to_PC_Basic(' ');
+waitforkeypress_Basic(); 
+Char_to_PC_Basic(' ');
 
 random_num = PRN_16bit_GEN (random_num, &PRN_counter);
  Short_random_number = PRN_8bit_GEN();}
+ 
+ 
+ Re_order_array(num_array,40);
+}
 return 1;}
