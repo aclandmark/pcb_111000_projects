601b375b66ea6d45eb323e43130f5f8fa6d1458e
aclandmark <mark.osborne15@btinternet.com>
2024-12-23 20:57:20 +0000

    Update Local_subroutines.c

--- Arduino Projects/4_Display_Driver_Basic/5_Moving_display/Local_subroutines.c
+++ Arduino Projects/4_Display_Driver_Basic/5_Moving_display/Local_subroutines.c
@@ -1,76 +1,39 @@
 
-char waitforkeypress_Basic (void);
-char decimal_digit_Basic (char );
-
-void display_num_string (const char*, int);
-void I2C_Tx_any_segment_clear_all(void);
-void I2C_Tx_any_segment(char, char);
+#include <util/delay.h>
+#include <avr/wdt.h>
+#define wdr()  __asm__ __volatile__("wdr")
 
 
-#define zero "abcdef"          //chars a,b,c,d,e,f stored in array named "zero"
-#define one "bc"
-#define two "abged"
-#define three "abcdg"
-#define four "fgbc"
-#define five "afgcd"
-#define six "fegdc"
-#define seven "abc"
-#define eight "abcdefg"
-#define nine "abcfg"
-#define null "\0"
-
-extern const char* string_ptr;
+char wait_for_return_key_Basic(void);
+char decimal_digit_Basic (char);
+void I2C_Tx_any_segment_clear_all (void);
+void I2C_Tx_8_byte_array (char*);
 
 
+char Int_KBD_to_display_A_Local(char display_buffer[]){                //Acquires an integer string from the keyboard and returns the binary equivalent
+char keypress;
+long Long_Num_from_mini_OS;
+char keypress_counter = 0;
 
-char digit_array_to_display(char digit_array[], char mode)    //"digit_array" is a pointer to a block of memory defined by "main"
-{char keypress;
-int array_offset = 0;                     //takes on values between 0 and 16                      
-
-do{
-if(mode == 1)                         //mode = 1: First of all enter keypresses into "digit_array"
-{keypress = waitforkeypress_Basic();                  //Only keypresses 0 to 9 or x allowed. Otherwise use "continue" to skip input 
-if((decimal_digit_Basic (keypress)) || (keypress == 'x'))     //mode = 0: Skip keypress entry stage
-digit_array[array_offset] = keypress;         
-else continue;}
-
-switch(digit_array[array_offset]){                //Read entries stored in "digit_array" and send them to the display.
-case '0': string_ptr = zero; break;               //next keypress in "digit_array" is '0': "string_ptr" addresses string "zero" 
-case '1': string_ptr = one; break;
-case '2': string_ptr = two; break;
-case '3': string_ptr = three; break;
-case '4': string_ptr = four; break;
-case '5': string_ptr = five; break;
-case '6': string_ptr = six; break;
-case '7': string_ptr = seven; break;
-case '8': string_ptr = eight; break;
-case '9': string_ptr = nine; break;
-case 'x': digit_array[array_offset] = 0; return 0; break;   //Inform "main" that user wants to enter < 8 digits
-case 0: string_ptr = null; break;}                //All unused bytes in "digit_array" contain zero: Null generates a space.
-
-{display_num_string(string_ptr, array_offset++);}}        //Increment "array_offset" before executing next line of code
-while (array_offset < 8);                   //Exit "do" loop when "array_offset" gets to 8
-
-return 1;}                            //Inform "main" that user has entered 8 digits
-
-
-
-/************************************************************************************************/
-void display_num_string (const char* s, int digit_num){       //Subroutine requires a pointer to the string   
-int char_ptr=0;                         //containing segments used to define a digit
-char letter;
+for(int n = 0; n< 16; n++) display_buffer[n] = 0;              
 
 while(1){
-letter = *(s + char_ptr);                   //Address of first segment of next digit
-switch(letter){
-case 'a':
-case 'b': 
-case 'c': 
-case 'd': 
-case 'e': 
-case 'f': 
-case 'g': I2C_Tx_any_segment(letter, digit_num);break;      //update display
-case 0:  return; break;
-default: break;}char_ptr++;}}                 //incrementing "char_ptr" steps through the string
-                                //Selecting segment letters in turn until the null character:0 is encountered
-                                
+if ((keypress = wait_for_return_key_Basic())=='\r')break;         //Detect return key press (i.e \r or\r\n)
+if (!(decimal_digit_Basic(keypress)))continue;
+
+if(display_buffer[0]){for(int n = 15; n>=1; n--)                                      //Shift display for each new keypress
+display_buffer[n] = display_buffer[n-1];}
+display_buffer[0] = keypress;                                  //Add new keypress           
+
+I2C_Tx_8_byte_array(display_buffer);                           //Update display includes "cr_keypress"                                                 
+keypress_counter += 1;
+
+if (display_buffer[14])break;}
+
+display_buffer[keypress_counter] = 0;
+
+I2C_Tx_any_segment_clear_all();
+wdr();_delay_ms(50);wdr();_delay_ms(50);
+I2C_Tx_8_byte_array(display_buffer);
+
+return keypress_counter;}
