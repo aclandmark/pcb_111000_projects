7759491e066ae2f830d2cf85bac49db974793889
aclandmark <mark.osborne15@btinternet.com>
2024-12-24 17:58:26 +0000

    Update 6_Random_num_display.ino

--- Arduino Projects/4_Display_Driver_Basic/6_Random_num_display/6_Random_num_display.ino
+++ Arduino Projects/4_Display_Driver_Basic/6_Random_num_display/6_Random_num_display.ino
@@ -1,23 +1,63 @@
 
 #include "Random_num_header.h"
+#include "Local_subroutines.c"
+
 
   
   int main (void)   //Example 12
-  { unsigned int random_num;
+  { unsigned int random_num[2];
   unsigned char PRN_counter;
-  long PORT_1 = 1, PORT_2 = 1;
+  char display_string[8], num_string[8];
+  char num_length;
+    int num_counter=0;
 
   setup_HW;
-  wdt_enable(WDTO_30MS);
+  //wdt_enable(WDTO_30MS);
   PRN_counter = 0;
-  random_num = PRN_16bit_GEN (0, &PRN_counter);
-  while (1)
-  { for (int m = 0; m < random_num % 3; m++)
-    { if (!(PORT_1 = ((PORT_1 * 2) % 0x10000)))
-        PORT_1 = 1;}
-    if (!(PORT_2 = ((PORT_2 * 2) % 0x10000)))
-  PORT_2 = 1;
-    I2C_Tx_2_integers(PORT_1, PORT_2);
-    Timer_T0_10mS_delay_x_m(8);
+  random_num[0] = PRN_16bit_GEN (0, &PRN_counter);
+   random_num[1] = PRN_16bit_GEN 
+  (random_num[0], &PRN_counter);
+
+
+  
+  while (1)       
+  {
+    
+    while(switch_3_down)wdr();
+    num_length = Bin_to_string_Local(random_num[num_counter%2], num_string);
+    //num_length -= 1;
+    Num_string_to_PC_Local(num_string,num_length);newline_Basic();
+
+    
+    for(int n = 0; n <= num_length; n++){
+    display_string[0] = num_string[num_length - n];
+    I2C_Tx_8_byte_array(display_string);_delay_ms(100);
+    
+for(int m = 0; m < 7; m++){
+display_string[7-m] = display_string[7-m-1];}
+
+ 
+Timer_T0_10mS_delay_x_m(10);
+}
+  random_num[(num_counter+1)%2] = PRN_16bit_GEN 
+  (random_num[num_counter%2], &PRN_counter);
+
+num_counter += 1;
+  
+  
+  }
+
+
+
+   /* Char_to_PC_Basic('\t');
+    Int_to_PC_Basic(num_length);
+    newline_Basic();
+    
+    Timer_T0_10mS_delay_x_m(25);while(switch_3_down)wdr();
     random_num = PRN_16bit_GEN 
-  (random_num, &PRN_counter);}}  
+  (random_num, &PRN_counter);*/
+  
+  
+  
+  
+  }  
