e91631195d5812b3dabaa53c45e872565b2b3bb2
aclandmark <mark.osborne15@btinternet.com>
2024-12-27 20:57:47 +0000

    Update Local_subroutines.c

--- Arduino Projects/4_Display_Driver_Basic/6_Random_num_display/Local_subroutines.c
+++ Arduino Projects/4_Display_Driver_Basic/6_Random_num_display/Local_subroutines.c
@@ -1,6 +1,9 @@
 
 
 void Char_to_PC_Basic(char);
+unsigned char PRN_8bit_GEN(void);
+unsigned int PRN_16bit_GEN(unsigned int, unsigned char *);
+void Int_to_PC_Basic (long );
 
 
 
@@ -19,3 +22,26 @@ int Bin_to_string_Local(long number, char *s){
 void Num_string_to_PC_Local(char *s, int str_len){
  for (int m = str_len; m > 0; m--)
  Char_to_PC_Basic(s[m - 1]);}
+
+
+
+
+
+ unsigned int new_random_number 
+ (unsigned int * sixteen_bit_PRN, unsigned char * PRN_counter)
+  
+  {unsigned int Eight_bit_PRN; 
+ 
+  * sixteen_bit_PRN = PRN_16bit_GEN (* sixteen_bit_PRN, PRN_counter);
+    Eight_bit_PRN = PRN_8bit_GEN();
+
+switch(Eight_bit_PRN%5)
+ {case 0: return *sixteen_bit_PRN%10; break;
+  case 1: return *sixteen_bit_PRN%100; break;
+  case 2: return *sixteen_bit_PRN%1000; break;
+  case 3: return *sixteen_bit_PRN%10000; break;
+  case 4: return *sixteen_bit_PRN%100000; break;}}
+
+
+
+/***************************************************************************************/
