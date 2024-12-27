42297bb9a0a54dff679b4bbfa620038237c21d0e
aclandmark <mark.osborne15@btinternet.com>
2024-12-27 20:57:40 +0000

    Update 6_Random_num_display.ino

--- Arduino Projects/4_Display_Driver_Basic/6_Random_num_display/6_Random_num_display.ino
+++ Arduino Projects/4_Display_Driver_Basic/6_Random_num_display/6_Random_num_display.ino
@@ -4,60 +4,37 @@
 
 
   
-  int main (void)   //Example 12
-  { unsigned int random_num[2];
-  unsigned char PRN_counter;
+  int main (void)
+  { unsigned int sixteen_bit_PRN;
+    unsigned int random_num[2];
+  unsigned char PRN_counter=0;
   char display_string[8], num_string[8];
   char num_length;
     int num_counter=0;
+    
 
   setup_HW;
-  //wdt_enable(WDTO_30MS);
+  
   PRN_counter = 0;
   random_num[0] = PRN_16bit_GEN (0, &PRN_counter);
-   random_num[1] = PRN_16bit_GEN 
-  (random_num[0], &PRN_counter);
-
-
+   
+sixteen_bit_PRN = random_num[0];
   
   while (1)       
-  {
-    
-    while(switch_3_down)wdr();
-    num_length = Bin_to_string_Local(random_num[num_counter%2], num_string);
-    //num_length -= 1;
-    Num_string_to_PC_Local(num_string,num_length);newline_Basic();
+  { num_length = Bin_to_string_Local(random_num[num_counter%2], num_string);
 
-    
     for(int n = 0; n <= num_length; n++){
     display_string[0] = num_string[num_length - n];
     I2C_Tx_8_byte_array(display_string);_delay_ms(100);
     
 for(int m = 0; m < 7; m++){
 display_string[7-m] = display_string[7-m-1];}
-
  
-Timer_T0_10mS_delay_x_m(10);
-}
-  random_num[(num_counter+1)%2] = PRN_16bit_GEN 
-  (random_num[num_counter%2], &PRN_counter);
+Timer_T0_10mS_delay_x_m(2);}
+ 
+random_num[(num_counter+1)%2] = new_random_number  
+ (&sixteen_bit_PRN, &PRN_counter);
 
-num_counter += 1;
+num_counter += 1;}}
   
   
-  }
-
-
-
-   /* Char_to_PC_Basic('\t');
-    Int_to_PC_Basic(num_length);
-    newline_Basic();
-    
-    Timer_T0_10mS_delay_x_m(25);while(switch_3_down)wdr();
-    random_num = PRN_16bit_GEN 
-  (random_num, &PRN_counter);*/
-  
-  
-  
-  
-  }  
