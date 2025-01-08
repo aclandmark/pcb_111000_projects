c521aea51f4e4f953626021411bf4e9b6b948e4d
aclandmark <mark.osborne15@btinternet.com>
2025-01-08 21:50:29 +0000

    Update F_Fraction_to_decimal.ino

--- Arduino Projects/8_Input_from_switches/F_Fraction_to_decimal/F_Fraction_to_decimal.ino
+++ Arduino Projects/8_Input_from_switches/F_Fraction_to_decimal/F_Fraction_to_decimal.ino
@@ -11,15 +11,61 @@ long Num_1, Num_2;
 char digits[8];
 
 setup_HW;
+
+sei();
+
 while(1){
-String_to_PC_Basic("Num_1?  ");
-Num_1 = Int_from_PC_Basic(digits); Int_to_PC_Basic(Num_1); newline_Basic();      
+String_to_PC_Basic("Num_1 and Num_2 ? (Num_2 > Num_1)\r\n");
 
-String_to_PC_Basic("Num_2?  ");
-Num_2 = Int_from_PC_Basic(digits);Int_to_PC_Basic(Num_2);newline_Basic();
+Num_1 = number_from_IO();     
 
-String_to_PC_Basic("Answer =   ");
+Num_2 = number_from_IO();
 
-if (Num_1 > Num_2)fraction_to_decimal_string_Local(Num_2, Num_1, digits); 
-else fraction_to_decimal_string_Local(Num_1, Num_2, digits); 
+fraction_to_decimal_string_Local(Num_1, Num_2, digits); 
 newline_Basic();}SW_reset;}
+
+
+
+
+
+
+/******************************************************************************************/
+long number_from_IO(void){
+
+set_up_PCI;
+enable_PCI;
+
+Init_display_for_pci_data_entry;                                    //Set digit[0] to zero and display it.
+while(!(Data_Entry_complete))wdr();                                      //Line A. wait here for pci interrupts used to enter data
+Data_Entry_complete = 0;
+disable_PCI;
+return I2C_displayToNum();}                                         //Acquire binary value of the display and return it.
+
+
+
+/********************************************************************************************************************/
+ISR(PCINT2_vect) {                                                //input number: store keypresses in array -digits
+if(switch_1_down)enable_PCI_on_sw3;
+while(switch_1_down){                                             //Use switch 1 to select keypress 0...9
+    switch(digits[0]){                                            //Test digits[0]
+      case '9': if(!(digits[1]))                                  //If its 9 scroll onto "-"
+      digits[0] = '-';                                            //but only if this is the first entry (i.e. digits[1] is 0.
+      else digits[0] = '0';break;                                 //otherwise scroll from zero to 9
+      case '-': digits[0] = '0';break;                             //If digits[0] is "-" scroll onto '0'.
+      default: digits[0]++; break;}                                //Otherwise increment it
+    I2C_Tx_8_byte_array(digits);                                  //Send value to the display
+    Timer_T0_10mS_delay_x_m(25);}                                 //return to Line A when sw1 is released
+
+if(switch_3_down) {disable_PCI_on_sw3;                           //Use switch 3 to shift display left
+    shift_digits_left; digits[0] = '0';                             //shift the display and initialise digit[0] (make it display '0')
+  I2C_Tx_8_byte_array(digits);}}                                  //update the display and return to Line A
+
+
+
+/********************************************************************************************************************/
+ISR(PCINT0_vect){
+if(switch_2_up)return;                                           //Ignore switch release
+I2C_Tx_any_segment_clear_all();
+Timer_T0_10mS_delay_x_m(25);                                      //Flash display
+I2C_Tx_8_byte_array(digits);
+Data_Entry_complete=1;}                                           //Return to Line A
