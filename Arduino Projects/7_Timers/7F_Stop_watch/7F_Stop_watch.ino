f216edee9875816ef04bc4d50e5bc83f50938774
aclandmark <mark.osborne15@btinternet.com>
2025-01-11 21:27:53 +0000

    Update 7F_Stop_watch.ino

--- Arduino Projects/7_Timers/7F_Stop_watch/7F_Stop_watch.ino
+++ Arduino Projects/7_Timers/7F_Stop_watch/7F_Stop_watch.ino
@@ -1,34 +1,32 @@
 
-/*Proj_7F_Stop_watch
-*********************************************************/
+//Proj_7F_Stop_watch
+ 
 
 
-/*IT INTRODUCES
+//IT INTRODUCES
 
-1. Stop watch (SW) provided by the mini-OS
+//1. Stop watch (SW) provided by the mini-OS
 
-2.  Project subroutines
-  I2C_Tx_Clock_command(one100ms_mode)       Initiates 100mS mode
-  I2C_Tx_Clock_command(ten_ms_mode)       Initiates 10mS mode
-  I2C_Tx_Clock_command(store_time)        Save time and pause display
-  I2C_Tx_Clock_command(display_stored_times)    Displays time saved to memory
-  I2C_Tx_Clock_command(display_current_time)    Resume stop watch mode
+//2.  Project subroutines
+  //I2C_Tx_Clock_command(one100ms_mode)       Initiates 100mS mode
+  //I2C_Tx_Clock_command(ten_ms_mode)       Initiates 10mS mode
+  //I2C_Tx_Clock_command(store_time)        Save time and pause display
+  //I2C_Tx_Clock_command(display_stored_times)    Displays time saved to memory
+  //I2C_Tx_Clock_command(display_current_time)    Resume stop watch mode
   
 
-USER INSTRUCTIONS
+//USER INSTRUCTIONS
 
-  sw3 selects 100ms clock, sw1 a 10ms clock
-  Press sw3 to pause SW and save time
-  Press sw1 the read back saved times
-  Pressing sw3 to restore SW
-  Press sw2 to re-initialise SW
+  //sw3 selects 100ms clock, sw1 a 10ms clock
+  //Press sw3 to pause SW and save time
+  //Press sw1 the read back saved times
+  //Pressing sw3 to restore SW
+  //Press sw2 to re-initialise SW
 
-Switch location SW1(PD2) - SW2(PD7) – SW3(PB2)*/
+//Switch location SW1(PD2) - SW2(PD7) – SW3(PB2)
 
 
 #include "Proj_7F_header_file_1.h"
-
-
 
 volatile char T1_ovf_flag = 0;                           //requires volatile label
                                                          //even though "T1_ovf_flag" is used in "main"
