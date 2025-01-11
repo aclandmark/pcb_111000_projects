4350d8b082a0de1f66e419a4551b3b6e0cc5f6ad
aclandmark <mark.osborne15@btinternet.com>
2025-01-11 21:28:30 +0000

    Update Local_subroutines.c

--- Arduino Projects/8_Input_from_switches/1_String_from_IO/Local_subroutines.c
+++ Arduino Projects/8_Input_from_switches/1_String_from_IO/Local_subroutines.c
@@ -1,10 +1,6 @@
 
 #include <avr/io.h>
-
-
 #include <stdio.h>
-
-
 #include <util/delay.h>
 
 #define switch_1_down ((PIND & 0x04)^0x04)
@@ -12,24 +8,18 @@
 void I2C_Tx_any_segment_clear_all(void);
 void I2C_Tx_8_byte_array(char *);
 
-
-
 extern char digits[8];
 
 void rotate_display(){   
-  
   char display_string[8];
   char num_length = 0;
   int p;
   
-
 for(int m = 0; m < 8; m++)if (!(digits[m]))break;
 else num_length += 1;
 
-
 I2C_Tx_any_segment_clear_all();
 for(int m = 0; m <=7; m++)display_string[m] = 0;
-
 p = 0;
 while(1){
 display_string[0] = digits[num_length - p%(num_length+1)]; p += 1;
@@ -40,3 +30,7 @@ display_string[7-m] = display_string[7-m-1];}
 
 if(switch_1_down) break;}
 }
+
+
+
+/************************************************************************/
