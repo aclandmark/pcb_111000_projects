297f350dd72943af4733e7eb4c6862480e6aca1f
aclandmark <mark.osborne15@btinternet.com>
2025-01-11 21:27:37 +0000

    Update 3_Square_roots.ino

--- Arduino Projects/8_Input_from_switches/3_Square_roots/3_Square_roots.ino
+++ Arduino Projects/8_Input_from_switches/3_Square_roots/3_Square_roots.ino
@@ -1,18 +1,5 @@
 
-/**************Proj_6G_Acquire_and process_Integer_Data_from_the user switches********************/
-
-/*
- * Prints colum of numbers each 1.5 times the previous until 99999999 or -9999999 is reached
- * then returns by dividing by 1.5
- * 
-*/
-
-/*
-EEPROM usage
-0x1FF, 0x1FE and 0x1FD     OSCCAL
-0x1FC and 0x1FB            PRN generator
-0x1FA                      Reset source
-*/
+//Calculates square roots of integer numbers
 
 #include "Square_roots_header.h"
 #include "square_root_subroutines.c"
@@ -25,34 +12,9 @@ sw2 to enter the number\r\nsw1 to pause the program and restart the program.\r\n
 #define message_2 "\r\nEnter new number\r\n"
 
 
-int main (void){
-long Num_1;
+//Enter the main routine here
 
-char digits[8];
-char result[8];
-int counter = 0;
 
-setup_HW;
-
-switch(reset_status){
-  case POR_reset:             break; 
-  case WDT_reset:             String_to_PC_Basic(message_2);break;
-   case External_reset:       String_to_PC_Basic(message_1);break;}
-
-sei();
-while((switch_1_down) || (switch_2_down) ||(switch_3_down));        //wait for switch release
-
-Num_1 = number_from_IO();
-I2C_Tx_long(Num_1);                                           //Sends number to the display
-Timer_T0_10mS_delay_x_m(15);
-
-root_computation(Num_1, digits);
-    Num_string_to_PC_Basic(digits); 
-    for(int m = 0; m < 8; m++)result[m] = digits[7-m];
-    I2C_Tx_8_byte_array(result);          
-
-while(switch_3_up);
-SW_reset;}
 
 
 
@@ -96,10 +58,6 @@ I2C_Tx_any_segment_clear_all();
 Timer_T0_10mS_delay_x_m(25);                                      //Flash display
 I2C_Tx_8_byte_array(digits);
 Data_Entry_complete=1;}                                           //Return to Line A
-
-
-
-/****************************************************************************************************************/
 
 
 
