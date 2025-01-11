815c732d99116ebe07d3e996ede26aed3d3d93f1
aclandmark <mark.osborne15@btinternet.com>
2025-01-11 21:27:48 +0000

    Update 1_String_from_IO.ino

--- Arduino Projects/8_Input_from_switches/1_String_from_IO/1_String_from_IO.ino
+++ Arduino Projects/8_Input_from_switches/1_String_from_IO/1_String_from_IO.ino
@@ -1,33 +1,13 @@
-
+//Enter a number from the IO.
+//It scrolls accross the display
+//Type in the main routine
 
 #include "INT_IO_to_display_header.h"
 #include "Local_subroutines.c"
 
 void rotate_display(void);
 
-
-
-int main (void){
-long Num_1;
-
-setup_HW;
-
-sei();
-while(switch_1_down) ;                                             //wait for switch release
-
-set_up_PCI;
-enable_PCI;
-
-Init_display_for_pci_data_entry;                                    //Set digit[0] to zero and display it.
-while(!(Data_Entry_complete));                                     //Line A. wait here for pci interrupts used to enter data
-Data_Entry_complete = 0;
-disable_PCI;
-Num_1 =  I2C_displayToNum();                                         //Acquire binary value of the display and return it.
-
-rotate_display();
-
-while(switch_1_up);
-SW_reset;}
+//Type main routine here
 
 
 
