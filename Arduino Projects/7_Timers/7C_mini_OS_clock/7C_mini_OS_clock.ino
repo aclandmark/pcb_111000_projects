880ca656a43625302c0e205b53372ca9b885d5a8
aclandmark <mark.osborne15@btinternet.com>
2025-01-11 21:28:04 +0000

    Update 7C_mini_OS_clock.ino

--- Arduino Projects/7_Timers/7C_mini_OS_clock/7C_mini_OS_clock.ino
+++ Arduino Projects/7_Timers/7C_mini_OS_clock/7C_mini_OS_clock.ino
@@ -1,29 +1,28 @@
 
-/*Proj_7C_Basic_crystal_clock  A crystal controlled clock provided by the mini-OS.
-******************************************************************************************/
+//Proj_7C_Basic_crystal_clock  A crystal controlled clock provided by the mini-OS.
 
 
-/*IT INTRODUCES
+//IT INTRODUCES
 
 
-Mini-OS clock command "I2C_Tx_OS_timer(AT_clock_mode, start_time)".
-An AT clock mode of 7 starts the clok running.
-The various modes (i.e. pause, hode etc.) are saved in the "clock_stop_watch .h" file.
+//Mini-OS clock command "I2C_Tx_OS_timer(AT_clock_mode, start_time)".
+//An AT clock mode of 7 starts the clok running.
+//The various modes (i.e. pause, hode etc.) are saved in the "clock_stop_watch .h" file.
 
 
-USER INSTRUCTIONS
+//USER INSTRUCTIONS
 
-Following a reset we get the "R   R   R......" prompt. Press R and enter the time which is then saved to EEPROM.
-With SW2 pressed it is now possible to transfer the project pcb to an independent 5V supply and the clock will automatically start 
-at the time entered when SW2 is released.
+//Following a reset we get the "R   R   R......" prompt. Press R and enter the time which is then saved to EEPROM.
+//With SW2 pressed it is now possible to transfer the project pcb to an independent 5V supply and the clock will automatically start 
+//at the time entered when SW2 is released.
 
-There is a problem: Use of the EEPROM means that the pcb powers up in either one of two modes so that the pcb
-can get out of step with what the user expects.
-A reset with SW3 pressed will reset the EEPROM
+//There is a problem: Use of the EEPROM means that the pcb powers up in either one of two modes so that the pcb
+//can get out of step with what the user expects.
+//A reset with SW3 pressed will reset the EEPROM
 
-If time is not requested as expected press the reset button.
+//If time is not requested as expected press the reset button.
 
- */
+
 
 
 
