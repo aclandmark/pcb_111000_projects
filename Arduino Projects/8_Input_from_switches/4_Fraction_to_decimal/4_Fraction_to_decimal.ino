3276508857208b1d3d2dbeddf7a4c86487df31b8
aclandmark <mark.osborne15@btinternet.com>
2025-01-11 21:27:31 +0000

    Update 4_Fraction_to_decimal.ino

--- Arduino Projects/8_Input_from_switches/4_Fraction_to_decimal/4_Fraction_to_decimal.ino
+++ Arduino Projects/8_Input_from_switches/4_Fraction_to_decimal/4_Fraction_to_decimal.ino
@@ -1,28 +1,14 @@
 
+//Eplores the divide fumction in some detai
+
+
 #include "decimal_fraction_header.h"
 #include "Local_subroutines.c"
-
 
 void fraction_to_decimal_string_Local(long, long, char *);
 
 
-int main (void){
-long Num_1, Num_2;
-char digits[8];
-
-setup_HW;
-
-sei();
-
-while(1){
-String_to_PC_Basic("Num_1 and Num_2 ? (Num_2 > Num_1)\r\n");
-
-Num_1 = number_from_IO();     
-
-Num_2 = number_from_IO();
-
-fraction_to_decimal_string_Local(Num_1, Num_2, digits); 
-newline_Basic();}SW_reset;}
+//Type in main routine here
 
 
 
