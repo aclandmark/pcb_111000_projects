0daa8db2910368208ee770e0a20299969286390a
aclandmark <mark.osborne15@btinternet.com>
2025-01-13 20:37:39 +0000

    Update 2_Pin_Change_Interrupt.ino

--- Arduino Projects/5_Interrupts/2_Pin_Change_Interrupt/2_Pin_Change_Interrupt.ino
+++ Arduino Projects/5_Interrupts/2_Pin_Change_Interrupt/2_Pin_Change_Interrupt.ino
@@ -17,34 +17,7 @@ char switch_control;                                    //Temporarily disables s
 volatile unsigned int mask;                             //Records which leds haver been shot down
 
 
-int main (void)
-{ setup_HW;
-  _delay_ms(10);
-  sei();
-  set_up_PCI_on_sw2;
-  enable_pci_on_sw2;
-
-  mask = 0xFFFF;                                        //No leds shot down yet
-  
-  while (mask)                                          //Mask is zero when last led has been shot down
-  { switch_control = 1;                                 //Disable switch and 
-    Timer_T0_10mS_delay_x_m(20);                         //pause at start of each scan
-    scan = 1;                                           //Enable scan
-    PORT_1 = 1;                                         //Illuminate RH leds at start of new scan
-    while (scan)
-    { if (PORT_1 == 1)switch_control = 0;               //Enable switch 2 at start of new scan
-      I2C_Tx_2_integers
-      (PORT_1 & mask, (~mask) ^ PORT_1);                 //Update display as leds are shot down
-      Timer_T0_10mS_delay_x_m(10);
-      if (PORT_1 & 0x8000)scan = 0;                     //Most LH leds illuminated: Pause scan
-      else
-        PORT_1 = (PORT_1 << 1);                         //Illuminate next set of LEDS moving left 
-    }
-  }
-  I2C_Tx_2_integers(0, 0xFFFF);                    //Pause display before starting again
-  Timer_T0_10mS_delay_x_m(200);
-  SW_reset;
-}
+//Type main routine here.
 
 
 ISR(PCINT0_vect)
