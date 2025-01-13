79ddb4b1c897a46c6fcd6f8aa824aa9802292c9c
aclandmark <mark.osborne15@btinternet.com>
2025-01-13 20:37:50 +0000

    Update 5_File_download_with_interrupt.ino

--- Arduino Projects/5_Interrupts/5_File_download_with_interrupt/5_File_download_with_interrupt.ino
+++ Arduino Projects/5_Interrupts/5_File_download_with_interrupt/5_File_download_with_interrupt.ino
@@ -4,25 +4,7 @@
 volatile long PORT_1 = 1;  
 #define ctlz  0x1a
 
- int main (void)   
-  {char keypress;
-  setup_HW;
-
-  sei();
-  T1_clock_tick(100);
-
-if(watch_dog_reset != 1)String_to_PC_Basic("Send file: Ctrl-Z when finished\r\n");
-  
-while (!(isCharavailable_Basic(65)))
-    Char_to_PC_Basic('?');  
-  newline_Basic();
-Char_to_PC_Basic(Char_from_PC_Basic());
-
-while ((keypress = waitforkeypress_Basic()) != ctlz)Char_to_PC_Basic(keypress);
- String_to_PC_Basic("\r\nDone: AK to repeat\r\n");
-SW_reset;
-    }
-
+//Type main routine here
 
 
 void T1_clock_tick(int T1_period_in_ms)                               //Start the T1 clock
