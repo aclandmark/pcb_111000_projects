945f0464ec800af5fe927934757ceb2025570f68
aclandmark <mark.osborne15@btinternet.com>
2025-01-13 20:37:55 +0000

    Update 4B_Rx_interrupt.ino

--- Arduino Projects/5_Interrupts/4_Rx_interrupts/4B_Rx_interrupt/4B_Rx_interrupt.ino
+++ Arduino Projects/5_Interrupts/4_Rx_interrupts/4B_Rx_interrupt/4B_Rx_interrupt.ino
@@ -3,33 +3,8 @@ volatile char sym_counter;
 
 #include "4B_Rx_interrupt_header.h"
 
-int main (void)
-  { char symbol;
-    char q = 0;
-    int n = 0;
-    
-  setup_HW;
 
-  if (watch_dog_reset != 1)
-newline_Basic();
-
-    UCSR0B |= (1 << RXCIE0);
-  sei();
-newline_Basic();
-  symbol = '!';
-  while (symbol <= '~')
-  { Char_to_PC_Basic(symbol);
-    _delay_ms(75);
-    symbol++;
-    sym_counter++;
-    wdr();
-  }
-  cli();waitforkeypress_Basic();
-  
-newline_Basic();
-SW_reset;}
-
-
+//Type main here
 
 
 ISR(USART_RX_vect){
