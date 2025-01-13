02091240c1417b3036622823176fa183653b33bf
aclandmark <mark.osborne15@btinternet.com>
2025-01-13 20:38:00 +0000

    Update 4A_Rx_interrupt.ino

--- Arduino Projects/5_Interrupts/4_Rx_interrupts/4A_Rx_interrupt/4A_Rx_interrupt.ino
+++ Arduino Projects/5_Interrupts/4_Rx_interrupts/4A_Rx_interrupt/4A_Rx_interrupt.ino
@@ -6,30 +6,8 @@ char repeats[20];
 
 #include "4A_Rx_interrupt_header.h"
 
-int main (void)
-  { char symbol;
-    char q = 0;
-    int n = 0;
-    
-  setup_HW;
 
-    UCSR0B |= (1 << RXCIE0);
-  sei();
-
-  do {
-  symbol = 0;
-  while (symbol <= '~' - '!')
-  { Char_to_PC_Basic((symbol+q)%94 + '!');
-    _delay_ms(5);
-    symbol++;}
-    _delay_ms(50);
-q += 1;
-newline_Basic();
-}while(repeats[n++]);
- cli();waitforkeypress_Basic();
-newline_Basic();
-SW_reset;}
-
+//Type main here
 
 
 ISR(USART_RX_vect)
