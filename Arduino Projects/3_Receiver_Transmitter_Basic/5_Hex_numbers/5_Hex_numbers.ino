39c5b5d363dc842eedb939f4c5fd1d7207045314
aclandmark <mark.osborne15@btinternet.com>
2025-01-14 17:47:44 +0000

    Update 5_Hex_numbers.ino

--- Arduino Projects/3_Receiver_Transmitter_Basic/5_Hex_numbers/5_Hex_numbers.ino
+++ Arduino Projects/3_Receiver_Transmitter_Basic/5_Hex_numbers/5_Hex_numbers.ino
@@ -18,36 +18,9 @@ IT INTRODUCES
 
 char digits [12];
 
-int main (void){
-int Num;
-int Inc;
-unsigned char digit_array[4], radix = 16;
+//Type main routine here
 
-setup_HW;
 
-  String_to_PC_Basic("Enter positive number and increment then x or y or AK\r\n");  
-    Num = Int_from_PC_Basic(digits);
-    Hex_and_Int_to_PC_Basic(10, Num);
-    Inc = Int_from_PC_Basic(digits);
-  
-  Hex_and_Int_to_PC_Basic(10, Inc);
-  newline_Basic();
-    
-while(1){ 
-   Hex_and_Int_to_PC_Basic(10, Num);
-  Char_to_PC_Basic('\t');
- Hex_and_Int_to_PC_Basic(radix, Num);Char_to_PC_Basic('\t');     //Send the number to the pc with a radix of 16.
-  Binary_to_PC_local(Num);              //Send the number to the pc in binary form
-  String_to_PC_Basic("\r\n");
-  
-  switch(waitforkeypress_Basic()){
-   case 'x': Num= Num + Inc;break;
-   case 'y': Num= Num - Inc;break;
-   case 'z': Num = Num * -1; break;
-   default:SW_reset;break;    
-  }}
-  }
-  
   
 /**************************************************************************************************/
 void Binary_to_PC_local(int x){             //check with paper and pencil
@@ -56,8 +29,6 @@ for (int m = 0; m<16; m++){
   else Char_to_PC_Basic('0');   
   if(m%4==3)String_to_PC_Basic("  ");}}
 
+
+
 /*******************************************************************************************************/
-
-
-
-  /***************************************************************************************/
