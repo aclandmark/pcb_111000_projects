bf953230b446ff6a3d27c23209822086d8774c2e
aclandmark <mark.osborne15@btinternet.com>
2025-01-15 17:31:51 +0000

    Update 0_project_zero.ino

--- Arduino Projects/0_project_zero/0_project_zero.ino
+++ Arduino Projects/0_project_zero/0_project_zero.ino
@@ -1,11 +1,19 @@
 
 
-//Our very first project.
+//11 very basic projects that make the dual element LED flash and also:
+      //Take a general look at C programs 
+      //Explore
+          //The reset function 
+          //Memory
+          //Random numbers
+          //The user switches
 
 //Why not copy the C code from one or two examples in the folder "C_file_images\0_project_zero"
 //Start typing bellow the "#include" command.
 //People often use a project like one of these to test that everything has been set up correctly
-//especially if they are not familiiar with the microchip and programming hardware.
+//especially if they are not familiar with the microchip and programming hardware.
+
+
 
 #include "Project_0_header.h"
 
