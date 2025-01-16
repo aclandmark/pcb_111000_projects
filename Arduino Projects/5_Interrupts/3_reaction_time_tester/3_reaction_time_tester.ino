001335b7166c96cbac9b16388afddb8f175f4883
aclandmark <mark.osborne15@btinternet.com>
2025-01-16 17:23:01 +0000

    Update 3_reaction_time_tester.ino

--- Arduino Projects/5_Interrupts/3_reaction_time_tester/3_reaction_time_tester.ino
+++ Arduino Projects/5_Interrupts/3_reaction_time_tester/3_reaction_time_tester.ino
@@ -1,8 +1,8 @@
 
-/*
- Very similar to Project 3B.  
- Press sw2 when the led bars line up to make the display flash
- */
+
+//Very similar to Project 3B.  
+//Press sw2 when the led bars line up to make the display flash
+//But strangely does not include any interrupts
 
 #include "Reaction_time_tester_header.h"
 
