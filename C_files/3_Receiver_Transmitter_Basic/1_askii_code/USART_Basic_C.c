9768f1a33340278e07ab5932fa1ea0b9409ac05f
aclandmark <mark.osborne15@btinternet.com>
2025-01-21 17:01:07 +0000

    Update USART_Basic_C.c

--- C_files/3_Receiver_Transmitter_Basic/1_askii_code/USART_Basic_C.c
+++ C_files/3_Receiver_Transmitter_Basic/1_askii_code/USART_Basic_C.c
@@ -4,10 +4,11 @@ int main (void)                //Example 6
   { setup_HW;         //Exploring program memory
   char *mem_add;
  wdt_enable(WDTO_120MS);
-  while(switch_1_down)wdr();
+ while(switch_2_down)wdr();
   
   
-  String_to_PC_Local("\0Defining and using text strings\r\n");
+  String_to_PC_Local("\0Null string\r\n");
+  String_to_PC_Local("Defining and using text strings\r\n\r\n");
   const char *message_1 = "Hello world\r\n";
   const char *message_2 = "Sending text to a PC\r\n";
   const char message_3[] = "Writing C programs and\r\n";
@@ -18,15 +19,17 @@ int main (void)                //Example 6
   String_to_PC_Local(message_4);
   while(switch_1_up)wdr();
 
- String_to_PC_Local("\r\nPrint out of program memory\r\n\r\n");
+ String_to_PC_Local("Print out of program memory\r\n\r\n");
 
 mem_add = (char*)256;
-//Hex_and_Int_to_PC_Basic (10,256);
-for(int m = 0; m <=175; m++){
+
+for(int m = 0; m <=185; m++){
   if ((*(mem_add + m))== '\0')Hex_and_Int_to_PC_Basic (10,256+m);
  else Char_to_PC_Basic(*(mem_add + m));}
 
   newline_Basic();newline_Basic();
   
-  while(switch_1_down);
+  while(switch_1_down)wdr();
+ while(switch_2_up)wdr();
+  
   return 1;}
