f1ff67b3108a96f33209f681d11de49777fd5e0c
aclandmark <mark.osborne15@btinternet.com>
2025-01-23 21:30:29 +0000

    Update 1_Interrupt_basic.c

--- C_files/5_Interrupts/1_Interrupt_basic.c
+++ C_files/5_Interrupts/1_Interrupt_basic.c
@@ -62,3 +62,45 @@ ISR(TIMER1_COMPA_vect)                                            //Example 4
     m += 1;
     m = m % 8;
     initialise_display();}}}
+	
+	
+	
+	ISR(TIMER1_COMPA_vect)												//Example_6
+{
+  { OCR1A = 150 * 125;
+    TCNT1 = 0;
+
+ if (n < 5)
+      { PORT_1 |= PORT_1 << 1;
+        PORT_2 |= PORT_2 >> 1; }
+      else
+      { PORT_1 = PORT_1 << 1;
+        PORT_2 = PORT_2 >> 1; }
+
+  I2C_Tx_2_integers(PORT_1, PORT_2);
+  
+  n += 1;
+  if (n == n_max)
+  {  n = 0;
+    m += 1;
+    m = p % 8;
+    initialise_display();}}}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
