f153d26b4968736025c71743f595a9a579b11399
aclandmark <mark.osborne15@btinternet.com>
2025-02-18 21:45:39 +0000

    Update 4_Num_from_KBD.c

--- C_files/4_Display_Driver_Basic/4_Num_from_KBD.c
+++ C_files/4_Display_Driver_Basic/4_Num_from_KBD.c
@@ -1,20 +1,23 @@
 
 
-long Int_KBD_to_display_A_Local(char display_buffer[]){                //Acquires an integer string from the keyboard and returns the binary equivalent
+
+//Acquire an integer string from the keyboard and returns the binary equivalent
+long Int_KBD_to_display_A_Local(char display_buffer[]){                
+
 char keypress;
 long Long_Num_from_mini_OS;
 
 for(int n = 0; n<=8; n++) display_buffer[n] = 0;              
 
 while(1){
-if ((keypress = wait_for_return_key_Basic())=='\r')break;         //Detect return key press (i.e \r or\r\n)
+if ((keypress = wait_for_return_key_Basic())=='\r')break;  		//Detect return key press
 if (!(decimal_digit_Basic(keypress)))continue;
 
-if(display_buffer[0]){for(int n = 8; n>=1; n--)                                      //Shift display for each new keypress
+if(display_buffer[0]){for(int n = 8; n>=1; n--)       //Shift display for each new keypress
 display_buffer[n] = display_buffer[n-1];}
-display_buffer[0] = keypress;                                  //Add new keypress           
+display_buffer[0] = keypress;                         //Add new keypress
 
-I2C_Tx_8_byte_array(display_buffer);}                           //Update display includes "cr_keypress"                                                 
+I2C_Tx_8_byte_array(display_buffer);}       		//Update display includes "cr_keypress"
 
 I2C_Tx_any_segment_clear_all();
 wdr();_delay_ms(50);wdr();_delay_ms(50);
