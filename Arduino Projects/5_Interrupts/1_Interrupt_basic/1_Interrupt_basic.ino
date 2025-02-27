92662155a7fc5c9657e52816778dca9684857b41
aclandmark <mark.osborne15@btinternet.com>
2025-02-27 20:09:41 +0000

    Update 1_Interrupt_basic.ino

--- Arduino Projects/5_Interrupts/1_Interrupt_basic/1_Interrupt_basic.ino
+++ Arduino Projects/5_Interrupts/1_Interrupt_basic/1_Interrupt_basic.ino
@@ -6,8 +6,8 @@
 #include "Interrupt_basic_header.h"
 
 volatile long PORT_1 = 1, PORT_2 = 0x8000;                            //For example 1, 2 and 3
-volatile int m = 0, n = 0, n_max;                                            //Extras for example 3
-volatile unsigned int clock_rate = 150;                                //Extra for examples 2 and 3
+volatile int m = 0, n = 0, n_max;                                     //Extras for example 3
+volatile unsigned int clock_rate = 150;                               //Extra for examples 2 and 3
 volatile unsigned int PRN;                                            //For example 4
 unsigned char PRN_counter = 0;                                        //For example 4
 volatile unsigned char counter = 0;                                   //For example 4
@@ -30,11 +30,11 @@ while(1);
 
 
 //*****************************************************************************************************
-void T1_clock_tick(unsigned int T1_period_in_ms)                                 //Start the T1 clock
-{ TCNT1 = 0;
-OCR1A = T1_period_in_ms * 125;
-  TIMSK1 |= (1 <<  OCIE1A);
-  TCCR1B = 0x03;}
+void T1_clock_tick(unsigned int T1_period_in_ms)              //Sets up the T1 clockn (default value of T1_period_in_ms is 150) 
+{ TCNT1 = 0;                                                  //TCNT1 increases by 1 for each clock tick 
+OCR1A = T1_period_in_ms * 125;                                //Set register OCR1A to 150 x 125 = 18750
+  TIMSK1 |= (1 <<  OCIE1A);                                   //Generates an intrerrupt TCNT1 equals OCR1A (i.e. after 150mS) 
+  TCCR1B = 0x03;}                                             //Starts the clock with a speed of 125 ticks per mS
 
 
 
@@ -63,7 +63,20 @@ void initialise_display()
 
 
 //Type Timer1 ISR here
+ISR(TIMER1_COMPA_vect)                                             //Example 3
+  {OCR1A = clock_rate * 125;
+ TCNT1 = 0; 
+  I2C_Tx_2_integers(PORT_1 << m, PORT_2 >> m);
+  
+    if (!(n)) m += 1;
+    if (m == 16)n = 1;
+    if (n == 1)m -= 1;
+    if (m == 0)n = 0;}
 
+
+
+
+/***************************************************************************************************************/
 
 
 
