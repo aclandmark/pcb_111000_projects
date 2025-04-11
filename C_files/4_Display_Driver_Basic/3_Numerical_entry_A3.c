b82b9b07ec9cbc2c241c0c1cf099d83a6cb77b2d
aclandmark <mark.osborne15@btinternet.com>
2025-04-11 17:35:42 +0100

    Update 3_Numerical_entry_A3.c

--- C_files/4_Display_Driver_Basic/3_Numerical_entry_A3.c
+++ C_files/4_Display_Driver_Basic/3_Numerical_entry_A3.c
@@ -1,25 +1,20 @@
 
 
 int main (void){
-
 char   digit='0';
 int digit_num=0;                 					//defines number of next digit on display           
 int string_counter=0;
 int letter_counter=0;
 const char* string_ptr = 0;   						//pointer: will be loaded with the address of a segment string 
 													//(i.e. the address of string "zero", "one", "two" etc....) 
-
 setup_HW;               
-
 
 if(watch_dog_reset != 1){
 print_memory_contents;
 String_to_PC_Basic("\r\nSend digits?");}
 
 else String_to_PC_Basic("\r\nAgain");
-
 I2C_Tx_any_segment_clear_all();
-
 digit_num = 0;                              			//First digit on display
 
 do{                                         			//start of "do{}while();" loop
@@ -41,13 +36,11 @@ case '8': string_ptr = eight; break;
 case '9': string_ptr = nine; break;
 default: continue; break;} 									//Illegal key press: Go immediately to the start of the 
 															//do loop. 
-
 															//Send the address of the required string to 
 															//subroutine "display_num_string();"
 display_num_string(string_ptr, digit_num);digit_num++;} 
 while (digit_num < 8);   									//return to the top of the "do" loop until all digits 
 															//have been illuminated
-
 while(!(isCharavailable_Basic(1)))wdr(); 
 Char_from_PC_Basic();
 I2C_Tx_any_segment_clear_all();                				//clear display and repeat
