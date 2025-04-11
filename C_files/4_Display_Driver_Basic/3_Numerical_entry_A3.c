b87a40318943b5c8f07fd5234287a231089bacbe
aclandmark <mark.osborne15@btinternet.com>
2025-04-11 17:48:30 +0100

    Update 3_Numerical_entry_A3.c

--- C_files/4_Display_Driver_Basic/3_Numerical_entry_A3.c
+++ C_files/4_Display_Driver_Basic/3_Numerical_entry_A3.c
@@ -6,13 +6,11 @@ int digit_num=0;                 					//defines number of next digit on display
 int string_counter=0;
 int letter_counter=0;
 const char* string_ptr = 0;   						//pointer: will be loaded with the address of a segment string 
-													//(i.e. the address of string "zero", "one", "two" etc....) 
-setup_HW;               
-
+setup_HW;  											//(i.e. the address of string "zero", "one", "two" etc....) 
+             
 if(watch_dog_reset != 1){
 print_memory_contents;
 String_to_PC_Basic("\r\nSend digits?");}
-
 else String_to_PC_Basic("\r\nAgain");
 I2C_Tx_any_segment_clear_all();
 digit_num = 0;                              			//First digit on display
@@ -20,7 +18,6 @@ digit_num = 0;                              			//First digit on display
 do{                                         			//start of "do{}while();" loop
 while(!(isCharavailable_Basic(1)))wdr(); 
 digit = Char_from_PC_Basic();          					//user enters digit (0 to 9) at the PC keyboard
-
 switch(digit){            								//The appropriate address is loaded into location 
 														//"string_pointer"
 case '0': string_ptr = zero; break; 					//The address of array zero is loaded into 
@@ -34,11 +31,9 @@ case '6': string_ptr = six; break;
 case '7': string_ptr = seven; break;
 case '8': string_ptr = eight; break;
 case '9': string_ptr = nine; break;
-default: continue; break;} 									//Illegal key press: Go immediately to the start of the 
-															//do loop. 
+default: continue; break;} 									//Illegal key press: Go immediately to the start of the do loop
 															//Send the address of the required string to 
-															//subroutine "display_num_string();"
-display_num_string(string_ptr, digit_num);digit_num++;} 
+display_num_string(string_ptr, digit_num);digit_num++;}		//subroutine "display_num_string();"
 while (digit_num < 8);   									//return to the top of the "do" loop until all digits 
 															//have been illuminated
 while(!(isCharavailable_Basic(1)))wdr(); 
