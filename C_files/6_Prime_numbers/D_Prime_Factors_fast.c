43c13310d49ba50265014e70d92a8ba565ebeb18
aclandmark <mark.osborne15@btinternet.com>
2025-04-16 21:25:56 +0100

    Update D_Prime_Factors_fast.c

--- C_files/6_Prime_numbers/D_Prime_Factors_fast.c
+++ C_files/6_Prime_numbers/D_Prime_Factors_fast.c
@@ -1,5 +1,4 @@
 
-
 int main (void){
 long array_num=0;                                        //Zero for first array of 200 numbers, one for second array etc. 
 long array_size = 200, array_pointer=1;
@@ -8,18 +7,13 @@ int counter=0;
 long number;
 char num_string[12];
 
-                                                                                                                                    
  setup_HW;
-
 if(watch_dog_reset == 1)
 String_to_PC_Basic("\r\n?");
 else 
 String_to_PC_Basic("\r\nEnter number  ");
-
 number = Int_from_PC_Basic( num_string);
 
-
-//counter = 10;
 while(1){
 array_num = prime_no_generator(array_num, array_size,  array_of_nums);
 while(array_pointer < array_size)
@@ -30,11 +24,7 @@ counter += 1; if (!(counter%50)){counter = 0;Char_to_PC_Basic('.');}
 while (!(number%prime_num))
 {String_to_PC_Basic("A ");Int_to_PC_Basic(prime_num); 
 Char_to_PC_Basic(' '); 
-number /= prime_num;
-}
-if (number ==1)SW_reset;
-}}
-
+number /= prime_num;}
+if (number ==1)SW_reset;}}
 array_num += 1;
-array_pointer=0;
-}}
+array_pointer=0;}}
