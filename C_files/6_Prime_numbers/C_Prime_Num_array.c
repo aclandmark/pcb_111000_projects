18e6ec72398cb27cbab3bdb4702c5054c02547fd
aclandmark <mark.osborne15@btinternet.com>
2025-04-16 21:52:25 +0100

    Update C_Prime_Num_array.c

--- C_files/6_Prime_numbers/C_Prime_Num_array.c
+++ C_files/6_Prime_numbers/C_Prime_Num_array.c
@@ -1,4 +1,3 @@
-
 int main (void){
 long array_num=0;                                        //Zero for first array of 200 numbers, one for second array etc.
 int array_size = 200, array_pointer=1;
@@ -6,8 +5,7 @@ long num_array[200], prime_num;
 char counter=9;
 char num_string[12];
  
- setup_HW;
- 
+setup_HW;
 String_to_PC_Basic("Prime number array\r\n1  ");
 
 while(1){
@@ -26,7 +24,6 @@ String_to_PC_Basic("\r\n\r\nZero or next array or AOK  ");
 if(waitforkeypress_Basic()=='0')array_num += 1;
 else {String_to_PC_Basic("\t Array number?  ");
   array_num=Int_from_PC_Basic( num_string);}
-
 array_pointer=0;
 counter = 10;
 newline_Basic();}}
