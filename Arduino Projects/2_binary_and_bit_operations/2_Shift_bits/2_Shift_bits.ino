44bc161d8df68049667acccd8440429ee054c2ea
aclandmark <mark.osborne15@btinternet.com>
2025-04-21 12:46:33 +0100

    Update 2_Shift_bits.ino

--- Arduino Projects/2_binary_and_bit_operations/2_Shift_bits/2_Shift_bits.ino
+++ Arduino Projects/2_binary_and_bit_operations/2_Shift_bits/2_Shift_bits.ino
@@ -85,19 +85,7 @@ lfsr = (PRN_8bit_GEN ());                  //Generate a new PRN (0) tells subrou
 
 
 /*************************************************************************************/
-char logical_op(char X, char Y, char op_code) {
-  char result = 0;
-  switch (op_code) {
-
-    case '1': result = Y << X; break;
-    case '2': result = (byte)Y >> X; break;
-    case '3': result = Y | X; break;
-    case '4': result = Y & (~(X)); break;
-    case '5': result = Y ^ X; break;
-    case '6': result = Y & X; break;
-  }
-  return result;
-}
+//Type subroutine "logical_op" here
 
 
 
