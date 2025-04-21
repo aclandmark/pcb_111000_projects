6076b802570ab007ff649882c4183692d52005dc
aclandmark <mark.osborne15@btinternet.com>
2025-04-21 12:59:55 +0100

    Update 3_Char_to_binary.ino

--- Arduino Projects/2_binary_and_bit_operations/3_Char_to_binary/3_Char_to_binary.ino
+++ Arduino Projects/2_binary_and_bit_operations/3_Char_to_binary/3_Char_to_binary.ino
@@ -8,28 +8,7 @@ Uses the display to illustrate conversion between 8 bit binary and decimal numbe
 
 #include "Char_to_binary_header.h"
 
-char digit=1, op, mode;
-
-int main (void){
-
-op=0;
-mode = 'u';                                               //mode: signed or unsigned
-setup_HW;
-I2C_Tx_display_char(digit,mode);
-
-set_up_PCI;
-enable_pci;
-
-sei();
-
-while(1){
-switch(op){
-case 0: break;                                            //Static display
-case 1: I2C_Tx_display_char(++digit,mode);break;          //Increment "digit" before calling "I2C_Tx....."
-case 2: I2C_Tx_display_char(--digit, mode);break;}        //Decrement "digit" before calling "I2C_Tx....."
-
-Timer_T0_10mS_delay_x_m(15);}}
-
+//Type main routine here
 
 
 /**************************************************************************************************/
