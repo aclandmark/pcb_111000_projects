4fd765287793f680db516bd5c07bca2a53c1dad6
aclandmark <mark.osborne15@btinternet.com>
2025-08-22 17:04:08 +0100

    Update FPN_from_KBD_header.h

--- Arduino Projects/9_Real_numbers/A_FPN_from_KBD/FPN_from_KBD_header.h
+++ Arduino Projects/9_Real_numbers/A_FPN_from_KBD/FPN_from_KBD_header.h
@@ -2,6 +2,8 @@
 #include <avr/wdt.h>
 
 char watch_dog_reset = 0;
+char User_response;
+char str_counter;
 
 #define T0_delay_10ms 5,178
 #define T1_delay_100ms 3, 0x9E62
@@ -31,7 +33,9 @@ eeprom_write_byte((uint8_t*)(0x1FD),OSCCAL);\
 while (!(PIND & (1 << PD1)));\
 Timer_T0_10mS_delay_x_m(5);\
 OSC_CAL;\
-setup_PC_comms_Basic(0,16);\
+Serial.begin(115200);\
+while (!Serial);\
+sei();\
 I2C_Tx_LED_dimmer();\
 \
 \
@@ -41,8 +45,24 @@ I2C_TX_328_check();\
 waiting_for_I2C_master;\
 if (receive_byte_with_Nack()==1)\
 {TWCR = (1 << TWINT);\
-wdt_enable(WDTO_30MS);\
-I2C_Tx_display();}\
+Serial.write("\r\nPress\r\n\
+1 for OS version\r\n\
+2 for system data\r\n\
+3 Message from the OS (x to escape)\r\n\
+4 Default project\r\n\
+0 to escape\r\n");\
+switch (waitforkeypress_A()){\
+case '0':break;\
+case '1':I2C_Rx_get_version_A('0');break;\
+case '2':I2C_Rx_get_version_A('1');break;\
+case '3':str_counter = 0; do\
+{Read_Hello_world_string_A();newline_A();\
+str_counter += 1;\
+waitforkeypress_A();}\
+while (str_counter < 3);\
+break;\
+case '4':wdt_enable(WDTO_30MS);\
+I2C_Tx_display(); break;}}\
 else TWCR = (1 << TWINT);
 
 
@@ -129,11 +149,23 @@ TWCR = (1 << TWINT);
 
 
 
+/**********************************************************************************************************/
+#define User_prompt_A \
+while(1){\
+do{Serial.write("R?    ");}  while((isCharavailable_A (50) == 0));\
+User_response = Serial.read();\
+if((User_response == 'R') || (User_response == 'r'))break;} Serial.write("\r\n");
+
+
+
 /*****************************************************************************/
 #include "Resources_nano_projects/Subroutines/HW_timers.c"
-#include "Resources_nano_projects/PC_comms/Basic_Rx_Tx_Basic.c"
+#include "Resources_nano_projects/PC_comms/Basic_Rx_Tx_Arduino.c"
+#include "Resources_nano_projects/PC_comms/Arduino_Rx_Tx_UNO_pcb.c"
 #include "Resources_nano_projects/Chip2chip_comms/I2C_subroutines_1.c"
 #include "Resources_nano_projects/Chip2chip_comms/I2C_slave_Rx_Tx.c"
+#include "Resources_nano_projects/Subroutines/Real_num_resources.c"
+
 #include "Resources_nano_projects/Subroutines/Random_and_prime_nos.c"
 
 
