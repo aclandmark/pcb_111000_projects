4f106c4df277989f868df315b00114b49a90a762
aclandmark <mark.osborne15@btinternet.com>
2025-08-22 17:04:32 +0100

    Update A_FPN_from_KBD.c

--- C_files/9_Real_Numbers/A_FPN_from_KBD.c
+++ C_files/9_Real_Numbers/A_FPN_from_KBD.c
@@ -1,16 +1,16 @@
 
 
-long int_divide(long A, long B, int * binary_pt_counter)    
+long int_divide_Local(long A, long B, int * binary_pt_counter)    
   {
     long Div, mod;
     long Int_result;
 
  *binary_pt_counter = 0;
  
-real_divide(A, B, &Div, &mod);                      //Perform the first division to get the integer result plus remainder
+real_divide_Local(A, B, &Div, &mod);                      //Perform the first division to get the integer result plus remainder
  Int_result = Div;
 while (!(Int_result & 0x1000000))                   //Reserve 25 bits for the result of the division      
-{real_divide(mod*2, B, &Div, &mod);                 //Double the remainder and repeat the division.
+{real_divide_Local(mod*2, B, &Div, &mod);                 //Double the remainder and repeat the division.
  Int_result = Int_result <<1;                       //For each division shift result one place left
     if (Div) Int_result = Int_result | 1;           //If the result was one place a one in the bit just vacated
     *binary_pt_counter -= 1;}                            //The number of bits to the right of the binary point
@@ -23,8 +23,6 @@ while (!(Int_result & 0x1000000))                   //Reserve 25 bits for the re
 
 
 /*****************************************************************************************************************************/
-void real_divide(long A, long B, long *Div, long *mod)      
+void real_divide_Local(long A, long B, long *Div, long *mod)      
   {*Div = A/B;
   *mod = A%B;}
-
-  
