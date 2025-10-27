9a846c5b205d82cda699be4947626fb633dcc9b8
aclandmark <mark.osborne15@btinternet.com>
2025-10-27 21:30:38 +0000

    Update 5_Hex_numbers.ino

--- Arduino Projects/3_Receiver_Transmitter_Basic/5_Hex_numbers/5_Hex_numbers.ino
+++ Arduino Projects/3_Receiver_Transmitter_Basic/5_Hex_numbers/5_Hex_numbers.ino
@@ -1,6 +1,29 @@
 
 //Introducing hex numbers
 
+/*
+Humans feel at home with decimal numbers like 1250 for example
+But microchips can only process binary numbers 
+Using the PC calculator we find that 1250 is 0100 1110 0010
+Notice that it places the bits in groups of 4
+We can recognise 0100 and 0010 as 4 and 2 
+1110 is 14 but rather than writing 14 we write E.
+
+We can now write 1250 and 4E2 in hexadecimal
+It is not obvious the 1250 decimal is 010011100010
+But if we know that 1250 decimal is 4E2 in hex 
+we can write the binary equivalent quite easily.
+
+  Hex is a useful half way house between binary and decimal
+  Like decimal it is compact and it also gives the 
+  more cumbersome binary equivalent.
+  In addition to E we use the letters A,B,C,D and F for 10,11,12,13 and 15. 
+  Hex numbers are numbers to the base of 16.
+  At first glance they may not look particularly intuitive 
+  but many do find them useful.   
+*/
+
+
 
 #include "Num_types_header_file.h"
 #include "Local_subroutines.c"
